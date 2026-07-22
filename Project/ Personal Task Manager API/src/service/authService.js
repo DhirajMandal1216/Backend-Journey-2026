20c579584dfac848ff07bb972e324df2bd993d0e
Dhiraj <dhirajmandal1216@gmail.com>
2026-07-22 09:46:48 +0530

    feat: add bcrypt password hashing to auth

--- Project/ Personal Task Manager API/src/service/authService.js
+++ Project/ Personal Task Manager API/src/service/authService.js
@@ -1,18 +1,26 @@
 const User = require("../models/User");
 const { ValidationError, NotFoundError } = require("../errors/AppError");
+const bcrypt = require("bcrypt");
+
 const registerUser = async (data) => {
   const { name, email, password } = data;
   if (!name) throw new ValidationError("Name is required");
   if (!email) throw new ValidationError("Email is required");
   if (!password) throw new ValidationError("Password  is required");
-
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     throw new ValidationError("Email already exists");
   }
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await User.create(data);
-  return user;
+  const user = await User.create({ ...data, password: hashedPassword });
+
+  return {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 };
 
 const loginUser = async (data) => {
@@ -22,19 +30,17 @@ const loginUser = async (data) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    throw new NotFoundError("User not found");
+    throw new ValidationError("Invalid credentials");
   }
-
-  if (user.password !== password) {
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
     throw new ValidationError("Invalid credentials");
   }
 
   return {
-    user: {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-    },
+    id: user._id,
+    name: user.name,
+    email: user.email,
   };
 };
 
